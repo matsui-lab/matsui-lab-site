24319e57df84230247c0efd998705bce8cf2eebb
Yusuke Matsui <mail.to.matsui@gmail.com>
2025-11-24 16:16:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/matsui-lab-site/",   // ← GitHub Pages のURLに必ず必要
+  base: '/matsui-lab-site/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
